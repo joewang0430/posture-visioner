efe55f51253112d36d45b67232fec1207b36ef69
joewang0430 <joewj0430@gmail.com>
2025-01-18 17:00:24 -0500

    added navbar

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,12 @@
 
 import Link from 'next/link';
+import NavBar from '@/components/home/NavBar';
 
 export default function AboutPage() {
 
     return (
         <div>
+            <NavBar />
             <h1>This will intro about our project</h1>
             <Link href=".." className="underline">Click: Back</Link>
         </div>
--- src/app/animation/page.tsx
+++ src/app/animation/page.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
+import NavBar from '@/components/home/NavBar';
 
 export default function AnimationPage() {
     return(
         <div>
+            <NavBar />
             <h1>This will be the animation page</h1>
             <Link href=".." className="underline">Click: Back</Link>
         </div>
