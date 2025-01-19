02304a24f30311a3234a90877ff6ffcdb5f017cd
joewang0430 <joewj0430@gmail.com>
2025-01-18 19:10:08 -0500

    add video upload components

--- src/components/home/HomeBody.tsx
+++ src/components/home/HomeBody.tsx
@@ -1,11 +1,19 @@
+"use client"
 
 import Link from 'next/link';
+import FileUploader from './FileUploader';
 
 export default function HomeBody() {
     return(
     <main className="flex-grow container mx-auto py-8 px-4">
         <h1>Posture Visioner!</h1>
-        <Link href="/animation" className="underline">click: to Animation Page</Link>
+        <div>
+            <Link href="/animation" className="underline">click: to Animation Page</Link>
+        </div>
+        <div className='flex flex-row justify-between'>
+            <FileUploader />
+        </div>
+
     </main>
     );
 }
